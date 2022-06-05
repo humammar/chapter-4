098298adf337590352fe2b1bd94d49a94ea37854
Humam <humammarijan03@gmail.com>
2022-06-05 20:04:57 +0700

    finalisasi task1-clone

--- Task 1-clone/script.js
+++ Task 1-clone/script.js
@@ -1,14 +1,18 @@
 
 const button = document.getElementById("add");
-const paragraf1 = document.getElementById("p1")
+const paragraf1 = document.getElementById("p1");
+let idNumber = 2;
 
 
 button.onclick = function(){
     const parent = document.getElementById('wrapper');
     
     let clonedElement = paragraf1.cloneNode(true);
+
+    clonedElement.id ="p" +idNumber; ++idNumber;
+
     parent.append(clonedElement);
 
-    parent.appendChild(newElement);
+    // parent.appendChild(newElement);
     
 }
