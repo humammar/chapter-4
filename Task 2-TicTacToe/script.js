b6d035726dfe3d3a27fa33497deefec7005899d1
Humam <humammarijan03@gmail.com>
2022-06-06 00:08:44 +0700

    update task 2 - TicTacToe

--- Task 2-TicTacToe/script.js
+++ Task 2-TicTacToe/script.js
@@ -1,43 +1,84 @@
-const cross = "cross";
-const circle = "circle";
+const cross = "crossPlayer";
+const circle = "circlePlayer";
 
+// const boardElement = document.getElementById('.board')
 const boxElements = document.querySelectorAll(".box");
 const resetButton = document.getElementById("reset");
 
-let turn = cross;
+let isCrossPlayerTurn = true;
 
-resetButton.onclick = startGame;
 
-function placeMark(box, currentClass) {
-  box.classList.add(currentClass);
+start();
+restart.addEventListener('click', restartGame);
+
+function start() {
+  isCrossPlayerTurn = true;
+  box.forEach(box => {
+    const removeCrossPlayer = box.classList.remove(cross, disabled);
+    const removeCirclePlayer = box.classList.remove(circle, disabled);
+    box.addEventListener('click', handleClick, {once: true})
+  })
+}
+
+function restartGame() {
+  start();
 }
 
 function handleClick(e) {
   const boxTarget = e.target;
-
-    if (turn === cross) {
-        boxTarget.style.backgroundColor = "yellow";
-        turn = circle;
-    } else {
-        boxTarget.style.backgroundColor = "red";   
-        turn = cross; 
-    }
-
-//   placeMark(boxTarget, turn);
+  const currentClass = isCrossPlayerTurn ? cross : circle;
+  placeMark(boxTarget, currentClass);
+  swapTurn();
 }
 
-function startGame() {
-  boxElements.forEach((box) => {
-    box.addEventListener("click", handleClick, { once: true });
-  });
+function placeMark(box, currentClass) {
+  box.classList.add(currentClass, disabled);
 }
 
-startGame();
+function swapTurn() {
+  isCrossPlayerTurn = !isCrossPlayerTurn;
+}
 
 
 
+// resetButton.onclick = startGame;
 
-//     boxElements.forEach((box) => {
-//         box.addEventListener("click", handleClick);
-//     });
+// function placeMark(box, currentClass) {
+//   box.classList.add(currentClass);
 // }
+
+// function handleClick(e) {
+//   const boxTarget = e.target;
+//   if (turn === cross) {
+//       boxTarget.style.backgroundImage = cross;
+//       turn = circle;
+//   } else {
+//       boxTarget.style.backgroundImage = circle;   
+//       turn = cross; 
+//   placeMark(boxTarget, turn);
+// }
+
+// function startGame() {
+//   boxElements.forEach((box) => {
+//     box.addEventListener("click", handleClick, { once: true });
+//   });
+// }
+
+// startGame();
+
+
+
+
+// //     boxElements.forEach((box) => {
+// //         box.addEventListener("click", handleClick);
+// //     });
+// // }
+
+
+//     // if (turn === cross) {
+//     //     boxTarget.style.backgroundImage = cross;
+//     //     turn = circle;
+//     // } else {
+//     //     boxTarget.style.backgroundImage = circle;   
+//     //     turn = cross; 
+//     // }
