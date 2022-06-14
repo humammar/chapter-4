e3ea1577b829183b5baac41599871a62f003e030
Humam <humammarijan03@gmail.com>
2022-06-14 10:28:45 +0700

    final update sebelum close

--- Task 2-TicTacToe/script.js
+++ Task 2-TicTacToe/script.js
@@ -64,22 +64,71 @@ class Configuration {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
+  static turn;
+  static handleTurn(){
+    this.turn = this.turn !== this.cross ? this.cross : this.circle;
+  }
+}
+
+class Utilities {
+  static placemark(box, currentClass) {
+    box.classList.add(currentClass);
+  }
+
+  static checkWin(){
+    return Configuration.winningCombinations.some((combination) => {
+      return combination.every((index) => {
+        return Configuration.boxElements[index].classList.contains(Configuration.turn);
+      });
+    });
+  }
+
+  static checkDraw() {
+    return [...Configuration.boxElements].every((box) => {
+      return box.classList.contains(Configuration.cross) || box.classList.contains(Configuration.circle);
+    });
+  }
 }
 
 class Game {
   handleClick(e) {
+    const boxTarget = e.target;
+    // console.log(boxTarget);
+    Utilities.placemark(boxTarget, Configuration.turn);
+    if (Utilities.checkWin()) {
+      console.log(`${Configuration.turn} is the winner`);
+      Configuration.wrapper.append(`${Configuration.turn} is the winner`);
+
+    } 
+    else if (Utilities.checkDraw()) {
+      console.log(`the game is draw`);
+
+    }
+
+    Configuration.handleTurn();
 
   }
   
   
   
   startGame() {
+    Configuration.turn = Configuration.cross;
     Configuration.boxElements.forEach((box) => {
       box.classList.remove(Configuration.cross);
       box.classList.remove(Configuration.circle);
-      box.addEventListener("click", this.handleClick, { once: true});
+      box.addEventListener("click", this.handleClick, {once: true});
     });    
   }
+
+  endGame() {
+    Configuration.boxElements.forEach((box) => {
+      box.removeEventListener("click", this.handleClick, {once: true});
+      console.log(box);
+    })
+  }
+
+  
 }
 
 const game = new Game();
