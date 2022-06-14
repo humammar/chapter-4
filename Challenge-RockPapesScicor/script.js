ab8370a2431a0e943b4b7bf56edcfc7a3f0c8160
Humam <humammarijan03@gmail.com>
2022-06-14 13:27:09 +0700

    update progress challenge - Interface done, moving to script

---
+++ Challenge-RockPapesScicor/script.js
@@ -0,0 +1,20 @@
+let playerTurn = "player"
+let computerTurn = "computer"
+let winner = "player"
+
+if (winner === "player") {
+    let winResult = document.getElementById("winResult");
+    let win = document.createElement("p");
+    win.innerText = "Player Win";
+    winResult.append(win);
+} else if (winner === "computer") {
+    let winResult = document.getElementById("winResult");
+    let win = document.createElement("p");
+    win.innerText = "Com Win";
+    winResult.append(win);
+} else {
+    let winResult = document.getElementById("winResult");
+    let win = document.createElement("p");
+    win.innerText = "draw";
+    winResult.append(win);
+}
